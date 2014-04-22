78f005d24009db511edca00cdfa500b7f9573b61
jonavargas <jonavargas2012@gmail.com>
2014-04-22 11:40:48 -0600

    Varios Css

--- application/views/newPost.php
+++ application/views/newPost.php
@@ -9,8 +9,11 @@
 	<?php include('menu.php');?>
 
 	<?=form_open(base_url().'post/insertPost/')?>
-	<p>Title: <?=form_input('title')?></p>
-	<p>Content: <?=form_textarea('content')?></p>
-	<?=form_submit('submit', 'Create Post')?>
+	<div class='newPost'>
+		<div class='titleNewPost'><b>Title: <?=form_input('title')?></b></div>
+		<div class='lblContentNewPost'><b>Content:</b></div>
+		<div class='contentNewPost'><?=form_textarea('content')?></div>
+		<div class='btnNewPost'><?=form_submit('submit', 'Create Post')?></div>
+	</div>
 </body>
 </html>
--- application/views/showPosts.php
+++ application/views/showPosts.php
@@ -8,28 +8,32 @@
 <body id='back'>
 	<?php include('menu.php');?>
 
-	<?php if (!empty($posts)) : ?>
-		<?php foreach($posts as $post) : ?>
-			<h2><?=anchor(base_url().'post/viewPost/'.$post->id,$post->title)?></h2>
-			Content: <?=$post->content?><br />
-			Date: <?=$post->date?>
-		<?php endforeach; ?>
-	<?php else : ?>
-		<h1>No Post</h1>
-	<?php endif; ?>
+		<div class='divMainPost'>
+			<?php if (!empty($posts)) : ?>
+				<?php foreach($posts as $post) : ?>
 
+					<section class='divPost'>
+						<div class='titlePost'><h2><?=anchor(base_url().'post/viewPost/'.$post->id,$post->title)?></h2></div>
+						<div class='contentPost'><b><?=$string = $post->content?></b></div>								
+						<div class='datePost'><b><?=$post->date?></b></div>
+					</section>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<h1>No Post</h1>
+			<?php endif; ?>
+		</div>
 			<?php foreach ($data as $info):?>
-				<div class='title'><?php echo $info->title ?></div>
-	<div>
+		<div class='title'><?php echo $info->title ?></div>			
 		<div class='biography'>			
-			
-				<img class="picture" src="assets/img/jon.jpg"/>
-				<div class='info'><b>Biography: </b></div>
-				<div class='bioText'><?php echo $info->bio ?></div>
-				<div class='info'><b>Phone: </b></div>
-				<div class='phoneText'><?php echo $info->phone ?></div>
-				<div class='info'><b>Email: </b></div>
-				<div class='emailText'><?php echo $info->email ?></div>
+				<section >					
+					<img class="picture" src="assets/img/jon.jpg"/>
+					<div class='info'><b>Biography: </b></div>
+					<div class='bioText'><?php echo $info->bio ?></div>
+					<div class='info'><b>Phone: </b></div>
+					<div class='phoneText'><?php echo $info->phone ?></div>
+					<div class='info'><b>Email: </b></div>
+					<div class='emailText'><?php echo $info->email ?></div>
+				</section>
 			
 			 <section class="social"> 
 				<a href=<?php echo $info->facebook ?> target="_blank"><img class="soc" src="assets/img/facebook.PNG"/></a>
@@ -41,7 +45,6 @@
 				endforeach;
 			?>
 		</div>
-	</div>
 		
 </body>
 </html>
